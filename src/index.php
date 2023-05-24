6d787268d088f9c5958826e01743258580971c26
tebo <esteban.lima@gmail.com>
2023-05-24 14:07:03 -0400

    add php index

---
+++ src/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+         <H1><font color="white">Sample PHP application</font></H1>
+        <title>My First PHP Page</title>
+    </head>
+    <body>
+        <p>
+           <body bgcolor="#2C3E50">
+           <font color="white"> Hello. This is a sample PHP application!!</font>
+        </p>
+    </body>
+</html>
